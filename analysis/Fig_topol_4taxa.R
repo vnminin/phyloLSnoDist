d9a9b04e0ca829fc10fa1e5dee86f0f5cd0efdc9
peterbchi <peter.chi@villanova.edu>
2020-04-07 11:54:41 -0400

    4 taxa preliminary results

--- analysis/Fig_topol_4taxa.R
+++ analysis/Fig_topol_4taxa.R
@@ -1,4 +1,5 @@
 library(phyloLSnoDist)
+library(tictoc)
 
 set.seed(11312)
 
@@ -12,18 +13,28 @@ correct.tp.ml<-rep(0,n.scen)
 
 my.tree<-unroot(read.tree(here("analysis", "tree_files", "sim_phylo4-6.tree")))
 
+tic()
 for(s in 1:length(n.sites)){
   for(i in 1:reps){
     my.align <- simSeq(x = my.tree, l = n.sites[s])
 
     ols.tree <- phylo.ls(my.align, search.all=TRUE)
-    #    nodist.tree <- phylo.ls.nodist(my.align)
+    nodist.tree <- phylo.ls.nodist(my.align, search.all=TRUE)
     ml.tree <- phylo.ML(my.align, search.all=TRUE)
 
     correct.tp.ls[s] <- ifelse(all.equal(my.tree, ols.tree, use.edge.length = F), correct.tp.ls[s]+1, correct.tp.ls[s])
-    #    correct.tp.nodist[s] <- ifelse(all.equal(my.tree, nodist.tree, use.edge.length = F), correct.tp.nodist[s]+1, correct.tp.nodist[s])
+    correct.tp.nodist[s] <- ifelse(all.equal(my.tree, nodist.tree, use.edge.length = F), correct.tp.nodist[s]+1, correct.tp.nodist[s])
     correct.tp.ml[s] <- ifelse(all.equal(my.tree, ml.tree, use.edge.length = F), correct.tp.ml[s]+1, correct.tp.ml[s])
+
+    print(i)
 
   }
 }
+toc()
 
+save.image(here("analysis", "Fig_topol_4taxa.RData"))
+
+
+# 42 sec elapsed
+# for 2 reps. So 10000 should take 58 hours
+# 100 should take about half an hour. 4/6/20 try that now
