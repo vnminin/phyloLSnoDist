4f69a6e7e9d764c04bd9cb4af5b98693ab94a698
peterbchi <peter.chi@villanova.edu>
2020-04-08 16:00:24 -0400

    Could use a sanity check here

---
+++ R/K80problems_tmp.R
@@ -0,0 +1,13 @@
+library(phyloLSnoDist)
+
+set.seed(1)
+my.tree<-read.tree(here("analysis", "tree_files", "sim_phylo5-1.tree"))
+my.align <- simSeq(x = my.tree, l = 1000, Q = c(1, 4, 1, 1, 4, 1))
+
+ts.counts <- dist.dna(as.DNAbin(my.align), model="TS")
+tv.counts <- dist.dna(as.DNAbin(my.align), model="TV")
+
+ts.counts / tv.counts   # These will be around 2, NOT 4 as they should be...
+
+new.k80 <- new.ls.fit.K80(my.tree, my.align)
+new.k80$par.est[8] # This will be close to 4 as it should be
