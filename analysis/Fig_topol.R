70fe249b4468ab4e6c8b7cf24c73a42976db467f
peterbchi <peter.chi@villanova.edu>
2020-04-13 06:57:04 -0400

    new topology estimation results

--- analysis/Fig_topol.R
+++ analysis/Fig_topol.R
@@ -1,15 +1,12 @@
 library(here)
 source(here("analysis", "LCO_generator_all_n.R"))
 
-# Run the LCO generator for a sample size of reps (1000 currently)
-lco.bounds <- LCO.CI(reps,0.95,3)
-
 load(here("analysis","Fig_topol_4taxa.RData"))
 # 4/9/20 These indices are based on having run 6 scenarios and only needing 4. Redo if needed.
 results.4<-c(correct.tp.ls[1],correct.tp.nodist[1],correct.tp.ml[1],
              correct.tp.ls[2],correct.tp.nodist[2],correct.tp.ml[2],
-             correct.tp.ls[4],correct.tp.nodist[4],correct.tp.ml[4],
-             correct.tp.ls[6],correct.tp.nodist[6],correct.tp.ml[6])
+             correct.tp.ls[3],correct.tp.nodist[3],correct.tp.ml[3],
+             correct.tp.ls[4],correct.tp.nodist[4],correct.tp.ml[4])
 
 
 # Run the LCO generator for a sample size of reps (100 currently)
@@ -47,13 +44,13 @@ n.sites6 <- n.sites
 # 4, 5 and 6 taxa together
 
 pdf(here("analysis", "Fig_topol.pdf"),height=12,width=8)
-par(mfrow=c(3,1),oma=c(3,2,1,0),mar=c(1,2,2,2))
+par(mfrow=c(3,1),oma=c(4,2,5,0),mar=c(3,2,5,2))
 
 plot.new()
 plot.window(xlim=c(0.5,12.5),ylim=c(0,1.05),xaxs="i",yaxs="i")
 rect(3.5,-1,6.5,1.05,border="gray90",col="gray90")
 rect(9.5,-1,12.5,1.05,border="gray90",col="gray90")
-bxp(res4.box,outline=F,medpch=rep(c(16,17,18),4),medcex=1.6,whisklty=1,whisklwd=1.5,staplewex=1,staplelwd=1.5,boxwex=0.12,axes=F,add=T)
+bxp(res4.box,outline=F,medpch=rep(c(16,17,18),4),medcex=1.7,medlty="blank",whisklty=1,whisklwd=1.5,staplewex=1,staplelwd=1.5,boxwex=0.12,axes=F,add=T)
 axis(2,at=seq(0,1,by=0.25),label=c(0,0.25,0.5,0.75,1))
 axis(1,at=seq(2,13,by=3),label=n.sites4)
 mtext("Percent correct",line=2.5,side=2)
@@ -69,9 +66,10 @@ plot.new()
 plot.window(xlim=c(0.5,12.5),ylim=c(0,1.05),xaxs="i",yaxs="i")
 rect(3.5,-1,6.5,1.05,border="gray90",col="gray90")
 rect(9.5,-1,12.5,1.05,border="gray90",col="gray90")
-bxp(res5.box,outline=F,medpch=rep(c(16,17),4),medcex=1.6,whisklty=1,whisklwd=1.5,staplewex=1,staplelwd=1.5,boxwex=0.12,axes=F,add=T)
+bxp(res5.box,outline=F,medpch=rep(c(16,17,18),4),medcex=1.7,medlty="blank",whisklty=1,whisklwd=1.5,staplewex=1,staplelwd=1.5,boxwex=0.12,axes=F,add=T)
 axis(2,at=seq(0,1,by=0.25),label=c(0,0.25,0.5,0.75,1))
 axis(1,at=seq(2,13,by=3),label=n.sites5)
+mtext("Percent correct",line=2.5,side=2)
 mtext("Sequence length (nucleotides)",side=1,line=2.5)
 mtext("Five taxa",cex=1.5,line=1.25)
 box()
@@ -83,9 +81,10 @@ plot.new()
 plot.window(xlim=c(0.5,12.5),ylim=c(0,1.05),xaxs="i",yaxs="i")
 rect(3.5,-1,6.5,1.05,border="gray90",col="gray90")
 rect(9.5,-1,12.5,1.05,border="gray90",col="gray90")
-bxp(res6.box,outline=F,medpch=rep(c(16,17),4),medcex=1.6,whisklty=1,whisklwd=1.5,staplewex=1,staplelwd=1.5,boxwex=0.12,axes=F,add=T)
+bxp(res6.box,outline=F,medpch=rep(c(16,17,18),4),medcex=1.7,medlty="blank",whisklty=1,whisklwd=1.5,staplewex=1,staplelwd=1.5,boxwex=0.12,axes=F,add=T)
 axis(2,at=seq(0,1,by=0.25),label=c(0,0.25,0.5,0.75,1))
 axis(1,at=seq(2,13,by=3),label=n.sites6)
+mtext("Percent correct",line=2.5,side=2)
 mtext("Sequence length (nucleotides)",side=1,line=2.5)
 mtext("Six taxa",cex=1.5,line=1.25)
 box()
